2a57bd7bbf9d5b1a13ef356881799f99ff1b3b07
valros76 <valerian.dufrene@gmail.com>
2025-05-19 11:04:56 +0200

    Add main.js for video facade event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
-  
+  const videoFacade = document.querySelector(`a[target="iframe-bande-annonce"]`);
+  const videoPlayBtn = document.querySelector("#videoPlayBtn");
+
+  videoPlayBtn.addEventListener("click", e => {
+    e.preventDefault();
+    videoFacade.classList.add("hidden");
+    e.stopPropagation();
+  });
 });
